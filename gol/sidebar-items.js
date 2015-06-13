bdbb65765e9fda80b638fed43958460e0b4193ce
Benjamin Fox <deadalus.ai@gmail.com>
2015-06-14 00:44:01 +1200

    Updating cargo documentation

--- gol/sidebar-items.js
+++ gol/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["plaintext",""]],"enum":[["Cell","Represents a single Cell, alive or dead"]],"struct":[["Grid","A grid of `Cell`s"],["RowIterator","Iterator for the rows in a `Grid`"],["World","Represents a Game of Life Grid + generation"]]});
+initSidebarItems({"struct":[["Grid","A grid of `Cell`s"],["RowIterator","Iterator for the rows in a `Grid`"],["World","Represents a Game of Life Grid + generation"]],"mod":[["plaintext",""]],"enum":[["Cell","Represents a single Cell, alive or dead"]]});
