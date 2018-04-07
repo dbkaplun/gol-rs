4627f906b62e489f2f8a725d23a4e9b2f180007b
Dan Kaplun <dbkaplun@gmail.com>
2018-04-06 21:42:15 -0400

    Simplify plaintext parsing

--- src/plaintext/mod.rs
+++ src/plaintext/mod.rs
@@ -1,52 +1,33 @@
 //! Module for parsing the [`PlainText`](http://conwaylife.com/wiki/PlainText) Game of Life
 //! file format into a `Grid`.
 
-mod padding;
-
-use grid::Cell::*;
 use grid::{Cell, Grid};
-
-use self::padding::Padding;
 
 use std::convert;
 use std::fmt;
 use std::io;
-use std::iter;
 use std::result;
 use std::vec::Vec;
 
 /// Struct for the contents of a `PlainText` format Game of Life file.
 ///
-/// # Optional padding syntax
-///
-/// The `PlainText` parser also provides a `Padding` extension, e.g.:
-///
 /// ```text
 /// !Name: Example
-/// !Padding: 5,10,5
 /// !
 /// .O.
 /// O.O
 /// .O.
 /// ```
 ///
-/// Resulting in the following padding being applied to the result:
-///
-/// | Top | Right | Bottom | Left |
-/// |-----|-------|--------|------|
-/// | 5   | 10    | 5      | 10   |
-///
 pub struct PlainText {
-    pub name: String,
     pub comment: String,
-    pub data: Grid,
+    pub grid: Grid,
 }
 
 /// Represents any errors which occur during the `PlainText` parsing process
 #[derive(Debug)]
 pub enum ParseError {
     Io(io::Error),
-    NameLineMissing,
     Invalid,
 }
 
@@ -55,7 +36,6 @@ impl fmt::Display for ParseError {
         use self::ParseError::*;
         match *self {
             Io(ref e) => write!(fmt, "I/O Error: {}", e),
-            NameLineMissing => write!(fmt, "Name line missing"),
             Invalid => write!(fmt, "Body contained invalid data"),
         }
     }
@@ -83,20 +63,9 @@ pub fn parse_plaintext<R>(reader: R) -> ParseResult
 where
     R: io::BufRead,
 {
-    #[derive(PartialEq)]
-    enum S {
-        Name,
-        Comment,
-        Body,
-    }
-
-    let mut state = S::Name;
-
-    let mut name = String::new();
     let mut comment = String::new();
     let mut rows = Vec::new();
     let mut width = 0;
-    let mut padding = Padding::new(0, 0, 0, 0);
 
     for line in reader.lines() {
         let line = try!(line);
@@ -105,38 +74,18 @@ where
             continue;
         }
 
-        if state == S::Name {
-            if !line.starts_with("!Name:") {
-                return Err(ParseError::NameLineMissing);
+        if line.starts_with('!') {
+            if !comment.is_empty() {
+                comment.push_str("\n");
             }
-            let line = sub_string_from(line, 6).unwrap_or("").trim();
-            name.push_str(line);
-            state = S::Comment;
-            continue;
-        }
-        if state == S::Comment {
-            if !line.starts_with('!') {
-                state = S::Body;
-            } else if line.starts_with("!Padding:") {
-                //special padding extension
-                let line = sub_string_from(line, 9).unwrap_or("").trim();
-                if let Ok(p) = line.parse() {
-                    padding = p;
-                }
-            } else {
-                if !comment.is_empty() {
-                    comment.push_str("\n");
-                }
-                let line = sub_string_from(line, 1).unwrap_or("").trim();
-                comment.push_str(line);
-            }
-        }
-        if state == S::Body {
+            let line = sub_string_from(line, 1).unwrap_or("").trim();
+            comment.push_str(line);
+        } else {
             let mut row = Vec::with_capacity(width);
             for c in line.trim().chars() {
                 match c {
-                    'O' => row.push(Live),
-                    '.' => row.push(Dead),
+                    'O' => row.push(Cell::Live),
+                    '.' => row.push(Cell::Dead),
                     _ => return Err(ParseError::Invalid),
                 }
             }
@@ -149,31 +98,11 @@ where
         }
     }
 
-    let grid = pad_and_create_grid(rows, width, padding);
+    let height = rows.len();
+    let cells = rows.into_iter().flat_map(|row| row).collect();
+    let grid = Grid::from_raw(width, height, cells);
 
-    Ok(PlainText {
-        name,
-        comment,
-        data: grid,
-    })
-}
-
-fn pad_and_create_grid(rows: Vec<Vec<Cell>>, width: usize, p: Padding) -> Grid {
-    let width = width + p.left + p.right;
-    let height = rows.len() + p.top + p.bottom;
-
-    let mut cells = Vec::with_capacity(width * height);
-    let dead_cells = |c| iter::repeat(Dead).take(c);
-
-    cells.extend(dead_cells(p.top * width));
-    for row in rows {
-        cells.extend(dead_cells(p.left));
-        cells.extend(row);
-        cells.extend(dead_cells(p.right));
-    }
-    cells.extend(dead_cells(p.bottom * width));
-
-    Grid::from_raw(width, height, cells)
+    Ok(PlainText { comment, grid })
 }
 
 #[cfg(test)]
@@ -215,10 +144,9 @@ O.
 
         let value = result.unwrap();
 
-        assert_eq!(value.name, "Tumbler");
-        assert_eq!(value.comment, "This is a comment");
-        assert_eq!(value.data.width(), 2);
-        assert_eq!(value.data.height(), 2);
+        assert_eq!(value.comment, "Name: Tumbler\n\nThis is a comment");
+        assert_eq!(value.grid.width(), 2);
+        assert_eq!(value.grid.height(), 2);
 
         #[cfg_attr(rustfmt, rustfmt_skip)]
         let expected = vec![
@@ -226,15 +154,14 @@ O.
             O, X,
         ];
 
-        for (left, right) in value.data.iter_cells().zip(expected) {
+        for (left, right) in value.grid.iter_cells().zip(expected) {
             assert_eq!(left.2, &right)
         }
     }
 
     #[test]
-    fn can_parse_simple_plaintext_with_padding() {
+    fn can_parse_simple_plaintext2() {
         const PLAINTEXT: &'static str = "!Name: Tumbler
-!Padding: 1,2
 !
 ! This is a comment
 !
@@ -252,20 +179,17 @@ O.
 
         let value = result.unwrap();
 
-        assert_eq!(value.name, "Tumbler");
-        assert_eq!(value.comment, "This is a comment\n");
-        assert_eq!(value.data.width(), 4 + 2);
-        assert_eq!(value.data.height(), 2 + 2);
+        assert_eq!(value.comment, "Name: Tumbler\n\nThis is a comment\n");
+        assert_eq!(value.grid.width(), 2);
+        assert_eq!(value.grid.height(), 2);
 
         #[cfg_attr(rustfmt, rustfmt_skip)]
         let expected = vec![
-            X, X, X, X, X, X,
-            X, X, X, O, X, X,
-            X, X, O, X, X, X,
-            X, X, X, X, X, X,
+            X, O,
+            O, X,
         ];
 
-        for (left, right) in value.data.iter_cells().zip(expected) {
+        for (left, right) in value.grid.iter_cells().zip(expected) {
             assert_eq!(left.2, &right)
         }
     }
@@ -291,10 +215,9 @@ O.
 
         let value = result.unwrap();
 
-        assert_eq!(value.name, "Glider");
-        assert_eq!(value.comment, "This is a comment");
-        assert_eq!(value.data.width(), 3);
-        assert_eq!(value.data.height(), 3);
+        assert_eq!(value.comment, "Name: Glider\n\nThis is a comment");
+        assert_eq!(value.grid.width(), 3);
+        assert_eq!(value.grid.height(), 3);
 
         #[cfg_attr(rustfmt, rustfmt_skip)]
         let expected = vec![
@@ -303,13 +226,13 @@ O.
             O, O, O,
         ];
 
-        for (left, right) in value.data.iter_cells().zip(expected) {
+        for (left, right) in value.grid.iter_cells().zip(expected) {
             assert_eq!(left.2, &right)
         }
     }
 
     #[test]
-    fn can_exclude_comment() {
+    fn can_parse_single_cell() {
         const PLAINTEXT: &'static str = "!Name: Tumbler\n.";
 
         let bytes = PLAINTEXT.to_string().into_bytes();
@@ -322,21 +245,17 @@ O.
 
         let value = result.unwrap();
 
-        assert_eq!(value.name, "Tumbler");
-        assert_eq!(value.comment, "");
-    }
-
-    #[test]
-    fn parse_fails_when_name_missing() {
-        const PLAINTEXT: &'static str = ".";
-
-        let bytes = PLAINTEXT.to_string().into_bytes();
-        let cursor = io::Cursor::new(bytes);
-        let read = io::BufReader::new(cursor);
-
-        let result = super::parse_plaintext(read);
-
-        assert!(!result.is_ok(), "Result is Ok");
+        assert_eq!(value.comment, "Name: Tumbler");
+        assert_eq!(value.grid.width(), 1);
+        assert_eq!(value.grid.height(), 1);
+        assert_eq!(
+            value
+                .grid
+                .iter_cells()
+                .map(|(_x, _y, &cell)| cell)
+                .collect::<Vec<_>>(),
+            vec![X]
+        );
     }
 
     #[test]
